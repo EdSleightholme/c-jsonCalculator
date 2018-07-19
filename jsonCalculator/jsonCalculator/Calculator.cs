4ec1ae9f00e7f032cfc4c8ddcce4094036fd78a1
ed <edward.sleightholme@gmail.com>
2018-07-19 16:46:58 +0100

    First Commit
    First Commit to Master
    Contains all of first implementation of calculator

---
+++ jsonCalculator/jsonCalculator/Calculator.cs
@@ -0,0 +1,324 @@
+﻿/*
+Author: Ed Sleightholme
+Last Updated:19/07/2018
+Purpose: Is a basic calculator. That takes in a json file containing a calcuation. Then creates a 
+json file with answer calcauated from path given
+  
+ 
+Future Plans
+1.Add better automated testing
+2.Add use of backets
+3.Add more advanced mathmatical processes(sin,cosine,tan etc)
+4.Add more options in processing calucations(more ways of + for example)
+*/
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace jsonCalculator
+{
+    class Calculator
+    {
+        // Constuctor that takes a filepath and does a calcuation on file recived and 
+        //produces a file using the outpath containing the answer
+        public Calculator(string filePath, string outputpath)
+        {
+            //load in json file
+            string jsonFile = FileReader(filePath);
+            //convert json file in to standised format
+            List<string> listOperations = StripStringToStandisedList(jsonFile);
+
+            if (listOperations[0] == "-")
+            {
+                //if start symbol is - change first number to negative and change symbol to a +
+                listOperations[0] = "+";
+                Double tempVal = 0;
+                Double.TryParse(listOperations[1], out tempVal);
+                listOperations[1] = (-tempVal).ToString();
+            }
+            else if (listOperations[0] == "+")
+            {
+                // nothing needed to be done
+            }
+            else
+            {
+                throw new System.ArgumentException("Json file given first symbol is not valid (must be  plus,sub,+,-)");
+            }
+            //do all indices operations
+            listOperations = DealWithIndicesCalucations(listOperations);
+            //do all divide operations
+            listOperations = DealWithDivCalucations(listOperations);
+            //do all multiply operations
+            listOperations = DealWithMultiCalucations(listOperations);
+            //do all plus operations
+            listOperations = DealWithPlusCalucations(listOperations);
+            //do all subtraction operations
+            listOperations = DealWithSubCalucations(listOperations);
+
+            //get answer
+            string answer = listOperations[1];
+            //write answer to json file
+            FileWriter(outputpath, answer);
+        }
+        //Deals with Indices calucations in a standised calcaulation list 
+        private List<string> DealWithIndicesCalucations(List<string> listOperations)
+        {
+            List<string> toReturn = new List<string>();
+            int count = 0;
+            bool skip = false;
+            //loop though calcuation string
+            foreach (string command in listOperations)
+            {
+                //find ^ symbol then preform indices operation on numbers on either side of symbol 
+                if (command == @"^" && count != 0 && count != listOperations.Capacity)
+                {
+                    //convert string to doubles
+                    Double.TryParse(listOperations[count - 1], out double prefix);
+                    Double.TryParse(listOperations[count + 1], out double suffix);
+                    toReturn.RemoveAt(toReturn.Count - 1);//remove unneeded number
+                    toReturn.Add((Math.Pow(prefix, suffix).ToString()));
+                    skip = true;//miss next number
+                }
+                else if (skip == false)
+                {
+                    toReturn.Add(listOperations[count]);
+                }
+                else
+                {
+                    skip = false;//skip adding a number
+                }
+                count = count + 1;
+            }
+
+            return toReturn;
+        }
+        //Deals with Division calucations in a standised calcaulation list 
+        private List<string> DealWithDivCalucations(List<string> listOperations)
+        {
+            List<string> toReturn = new List<string>();
+            int count = 0;
+            bool skip = false;
+            //loop though calcuation string
+            foreach (string command in listOperations)
+            {
+                //find / symbol and preform calucation on numbers on either side of it 
+                if (command == @"/" && count != 0 && count != listOperations.Capacity)
+                {
+                    //convert string to doubles
+                    Double.TryParse(listOperations[count - 1], out double prefix);
+                    Double.TryParse(listOperations[count + 1], out double suffix);
+                    toReturn.RemoveAt(toReturn.Count - 1);//remove unneeded number
+                    toReturn.Add((prefix / suffix).ToString());
+                    skip = true;//miss next number
+                }
+                else if (skip == false)
+                {
+                    toReturn.Add(listOperations[count]);
+                }
+                else
+                {
+                    skip = false; //skips number
+                }
+                count = count + 1;
+            }
+
+            return toReturn;
+        }
+        //Deals with multiply calucations in a standised calcaulation list 
+        private List<string> DealWithMultiCalucations(List<string> listOperations)
+        {
+            List<string> toReturn = new List<string>();
+            int count = 0;
+            bool skip = false;
+            //loop though command string
+            foreach (string command in listOperations)
+            {
+                //find * symbol and preform calucation on numbers on either side of it 
+                if (command == "*" && count != 0 && count != listOperations.Capacity)
+                {
+                    //convert string to double
+                    Double.TryParse(listOperations[count - 1], out double prefix);
+                    Double.TryParse(listOperations[count + 1], out double suffix);
+                    toReturn.RemoveAt(toReturn.Count - 1);
+                    toReturn.Add((prefix * suffix).ToString());
+                    skip = true;
+                }
+                else if (skip == false)
+                {
+                    toReturn.Add(listOperations[count]);
+                }
+                else
+                {
+                    skip = false;
+                }
+                count = count + 1;
+            }
+
+            return toReturn;
+        }
+        //Deals with subtaction calucations in a standised calcaulation list 
+        private List<string> DealWithSubCalucations(List<string> listOperations)
+        {
+            List<string> toReturn = new List<string>();
+            int count = 0;
+            bool skip = false;
+            //loop though list operations
+            foreach (string command in listOperations)
+            {
+                //find command - and perfrom operation on numbers either side
+                if (command == "-" && count != 0 && count != listOperations.Capacity)
+                {
+                    Double.TryParse(listOperations[count - 1], out double prefix);
+                    Double.TryParse(listOperations[count + 1], out double suffix);
+                    toReturn.RemoveAt(toReturn.Count - 1);
+                    toReturn.Add((prefix - suffix).ToString());
+                    skip = true;
+                }
+                else if (skip == false)
+                {
+                    toReturn.Add(listOperations[count]);
+                }
+                else
+                {
+                    skip = false;
+                }
+                count = count + 1;
+            }
+
+            return toReturn;
+        }
+        //Deals with addition calucations in a standised calcaulation list 
+        private List<string> DealWithPlusCalucations(List<string> listOperations)
+        {
+            List<string> toReturn = new List<string>();
+            int count = 0;
+            bool skip = false;
+            //loop though list of operations
+            foreach (string command in listOperations)
+            {
+                //at + operation add numbers either side together
+                if (command == "+" && count != 0 && count != listOperations.Capacity)
+                {
+                    Double.TryParse(listOperations[count - 1], out double prefix);
+                    Double.TryParse(listOperations[count + 1], out double suffix);
+                    toReturn.RemoveAt(toReturn.Count - 1);
+                    toReturn.Add((prefix + suffix).ToString());
+                    skip = true;
+                }
+                else if (skip == false)
+                {
+                    toReturn.Add(listOperations[count]);
+                }
+                else
+                {
+                    skip = false;
+                }
+                count = count + 1;
+            }
+
+            return toReturn;
+        }
+
+        private List<string> StripStringToStandisedList(String fileToProcess)
+        {
+            //trim down to operation number pairs
+            string pattern = @"([^a-zA-Z0-9.\/\*\-\+\^])";
+            //remove all unnessaacry text
+            var splitJson = Regex.Replace(fileToProcess, pattern, " ");
+            //cut down blank space
+            splitJson = Regex.Replace(splitJson, "  ", " ");
+            //make a list on spaces
+            string[] words = splitJson.Split(' ');
+
+            List<string> toReturn = new List<string>();
+            List<string> typeCommand = new List<string>();
+            int count = 0;
+            foreach (var word in words)
+            {
+                //switch statment??
+                if (word == "")
+                {
+
+                }//add a number
+                else if (Regex.IsMatch(word, @"^-?[0-9]\d*(\.\d+)?$") || Regex.IsMatch(word, @"^-?[0-9]\d*(\.\d+)?$"))
+                {
+                    toReturn.Add(word);
+                    typeCommand.Add("number");
+                }//look for plus symbols
+                else if ("plus" == word || "+" == word)
+                {
+                    toReturn.Add("+");
+                    typeCommand.Add("symbol");
+                }//look for suntraction symbols
+                else if ("sub" == word || "-" == word)
+                {
+                    toReturn.Add("-");
+                    typeCommand.Add("symbol");
+                }//look for multiply symbols
+                else if ("mult" == word || "*" == word)
+                {
+                    toReturn.Add("*");
+                    typeCommand.Add("symbol");
+                }//look for divide symbols
+                else if ("div" == word || "/" == word)
+                {
+                    toReturn.Add("/");
+                    typeCommand.Add("symbol");
+                }//look for incies symbols
+                else if ("indices" == word || "^" == word)
+                {
+                    toReturn.Add("^");
+                    typeCommand.Add("symbol");
+                }
+                else //if dont reconize symbol throw error
+                {
+                    throw new System.ArgumentException("Json file given contains unrecognized symbol (" + word + ") " +
+                        "Error at line " + ((count / 2) + 1) + " in json file", "original");
+                }
+                count += 1;
+            }
+            //check order operations is correct. Must be symbol number loop
+            count = 0;
+            foreach (string command in typeCommand)
+            {
+                if (0 == count % 2)
+                {
+                    if (command != "symbol")
+                    {
+                        throw new System.ArgumentException("Json file given is not in correct format. " +
+                            "Expected to be symbol,number ordering. Error at number location line " +
+                            ((count / 2) + 1).ToString() + " in json file", "original");
+                    }
+                }
+                else
+                {
+                    if (command != "number")
+                    {
+                        throw new System.ArgumentException("Json file given is not in correct format. " +
+                            "Expected to be symbol,number ordering. Error at symbol location line " +
+                            ((count / 2) + 1).ToString() + " in json file", "original");
+                    }
+                }
+                count++;
+            }
+            return toReturn;
+        }
+        //Takes a file path and returns a string
+        private string FileReader(String filePath)
+        {
+            var r = new StreamReader(filePath);
+            var myJson = r.ReadToEnd();
+            r.Close();
+            return (myJson);
+        }
+        //Takes a file path and writes a file there
+        private void FileWriter(String filePath, String toWrite)
+        {
+            var w = new StreamWriter(filePath);
+            w.Write(toWrite);
+            w.Close();
+        }
+
+    }
+}
---
+++ jsonCalculator/jsonCalculator/Program.cs
@@ -0,0 +1,189 @@
+﻿/*
+Author: Ed Sleightholme
+Last Updated:19/07/2018
+Purpose: Is a basic calculator. Tests class Calucator
+  
+ 
+ 
+Future 
+1.Add better automated testing
+2.Add use of backets
+3.Add more advanced mathmatical processes(sin,cosine,tan,square root etc)
+4.Add more options in processing calucations(more ways of + for example)
+*/
+using System;
+using System.IO;
+namespace jsonCalculator
+{
+    class Program
+    {
+        //Takes a file path and returns a string
+        static string FileReader(String filePath)
+        {
+            var r = new StreamReader(filePath);
+            var myJson = r.ReadToEnd();
+            r.Close();
+            return (myJson);
+        }
+
+        static void Main(string[] args)
+        {
+            //test to see if basic plus works
+            Calculator test = new Calculator("ExampleTestFiles/example1.json", "answers/answer1.json");
+            string temp = FileReader("answers/answer1.json");
+            Console.WriteLine("example1.json");
+            Console.WriteLine(temp);
+            if (temp != "2")
+            {
+                Console.WriteLine("FAIL");
+            }
+            else
+            {
+                Console.WriteLine("PASS");
+            }
+            //test to see if deciaml loading words
+            test = new Calculator("ExampleTestFiles/example2.json", "answers/answer2.json");
+            temp = FileReader("answers/answer2.json");
+            Console.WriteLine("example2.json");
+            Console.WriteLine(temp);
+            if (temp != "2.4")
+            {
+                Console.WriteLine("FAIL");
+            }
+            else
+            {
+                Console.WriteLine("PASS");
+            }
+            //see if basic subtraction works
+            test = new Calculator("ExampleTestFiles/example3.json", "answers/answer3.json");
+            temp = FileReader("answers/answer3.json");
+            Console.WriteLine("example3.json");
+            Console.WriteLine(temp);
+            if (temp != "-0.4")
+            {
+                Console.WriteLine("FAIL");
+            }
+            else
+            {
+                Console.WriteLine("PASS");
+            }
+            //see if basic multiplying works
+            test = new Calculator("ExampleTestFiles/example4.json", "answers/answer4.json");
+            temp = FileReader("answers/answer4.json");
+            Console.WriteLine("example4.json");
+            Console.WriteLine(temp);
+            if (temp != "7")
+            {
+                Console.WriteLine("FAIL");
+            }
+            else
+            {
+                Console.WriteLine("PASS");
+            }
+            //see if basic divivsion works
+            test = new Calculator("ExampleTestFiles/example5.json", "answers/answer5.json");
+            temp = FileReader("answers/answer5.json");
+            Console.WriteLine("example5.json");
+            Console.WriteLine(temp);
+            if (temp != "0.5")
+            {
+                Console.WriteLine("FAIL");
+            }
+            else
+            {
+                Console.WriteLine("PASS");
+            }
+            //see if using negative numbers works
+            test = new Calculator("ExampleTestFiles/example6.json", "answers/answer6.json");
+            temp = FileReader("answers/answer6.json");
+            Console.WriteLine("example6.json");
+            Console.WriteLine(temp);
+            if (temp != "-127.5")
+            {
+                Console.WriteLine("FAIL");
+            }
+            else
+            {
+                Console.WriteLine("PASS");
+            }
+            //see if order of calcuation works
+            test = new Calculator("ExampleTestFiles/example7.json", "answers/answer7.json");
+            temp = FileReader("answers/answer7.json");
+            Console.WriteLine("example7.json");
+            Console.WriteLine(temp);
+            if (temp != "-102.5")
+            {
+                Console.WriteLine("FAIL");
+            }
+            else
+            {
+                Console.WriteLine("PASS");
+            }
+
+            //see if basic indices works
+            test = new Calculator("ExampleTestFiles/example8.json", "answers/answer8.json");
+            temp = FileReader("answers/answer8.json");
+            Console.WriteLine("example8.json");
+            Console.WriteLine(temp);
+            if (temp != "27")
+            {
+                Console.WriteLine("FAIL");
+            }
+            else
+            {
+                Console.WriteLine("PASS");
+            }
+            //see if using invalid number causes custom error
+            try
+            {
+                Console.WriteLine("example9.json");
+                test = new Calculator("ExampleTestFiles/example9.json", "answers/answer9.json");
+                temp = FileReader("answers/answer9.json");
+                Console.WriteLine(temp);
+                Console.WriteLine("FAIL");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine("PASS");
+            }
+            //see if using invalid symbol/number causes custom error
+            try
+            {
+                Console.WriteLine("example10.json");
+                test = new Calculator("ExampleTestFiles/example10.json", "answers/answer10.json");
+                temp = FileReader("answers/answer10.json");
+                Console.WriteLine(temp);
+                Console.WriteLine("FAIL");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine("PASS");
+            }
+            //see if alternative symbol use works
+            test = new Calculator("ExampleTestFiles/example11.json", "answers/answer11.json");
+            temp = FileReader("answers/answer11.json");
+            Console.WriteLine("example11.json");
+            Console.WriteLine(temp);
+            if (temp != "-97.5")
+            {
+                Console.WriteLine("FAIL");
+            }
+            else
+            {
+                Console.WriteLine("PASS");
+            }
+
+            //see error caused by a maths error
+            Console.WriteLine("example12.json");
+            test = new Calculator("ExampleTestFiles/example12.json", "answers/answer12.json");
+            temp = FileReader("answers/answer12.json");
+            Console.WriteLine(temp);
+            Console.WriteLine("PASS");
+
+
+            Console.ReadLine();
+        }
+    }
+}
